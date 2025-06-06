45c6c17972caef7ef1847ac5baf4e55eeb17aefe
HandsomeC <50850974+YeongCC@users.noreply.github.com>
2025-06-06 19:46:48 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,3 @@
-// import '@web3modal/polyfills' // ✅ 保留这句
-
-// import React from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
-// import './index.css'
-
-// import { createWeb3Modal } from '@web3modal/wagmi/react'
-// import { wagmiConfig, projectId } from './lib/wallet'
-// import { WagmiProvider } from 'wagmi'
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// createWeb3Modal({
-//   wagmiConfig,
-//   projectId
-// })
-
-// const queryClient = new QueryClient()
-
-// createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <WagmiProvider config={wagmiConfig}>
-//       <QueryClientProvider client={queryClient}>
-//         <App />
-//       </QueryClientProvider>
-//     </WagmiProvider>
-//   </React.StrictMode>
-// )
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
